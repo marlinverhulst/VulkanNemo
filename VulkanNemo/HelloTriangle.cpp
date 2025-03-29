0b225e7eefe2e099eedabaccf5cd0ea4998aac23
marlinverhulst <marlinverhulst@gmail.com>
2025-03-29 14:46:44 +0100

    Created instance

---
+++ VulkanNemo/HelloTriangle.cpp
@@ -0,0 +1,28 @@
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTraingleApplication {
+public:
+	void run() {
+		initVulkan();
+		mainLoop();
+		cleanUp();
+
+	}
+
+private:
+	void initVulkan() {
+
+	}
+
+	void mainLoop() {
+
+	}
+
+	void cleanUp() {
+
+	}
+};
--- VulkanNemo/main.cpp
+++ VulkanNemo/main.cpp
@@ -1,15 +1,117 @@
 #define GLFW_INCLUDE_VULKAN
 #include <glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTraingleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanUp();
+
+    }
+
+private:
+    GLFWwindow* window;
+    VkInstance instance;
+
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vukan!", nullptr, nullptr);
+
+    }
+
+    void initVulkan() {
+        createInstance();
+
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+
+    }
+
+    void cleanUp() {
+        vkDestroyInstance(instance, nullptr);
+        glfwDestroyWindow(window);
+        glfwTerminate();
+
+    }
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+       
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+        
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create instance!");
+        }
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr); // Just get the amount of extensions so I can allocate the correct size for the array;
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "Available extensions: \n";
+        for (const VkExtensionProperties& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+        
+
+    
+
+
+
+
+    }
+};
+
 
 int main() {
-    glfwInit();
+    HelloTraingleApplication app;
+
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+
+
+  /*  glfwInit();
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
@@ -22,7 +124,7 @@ int main() {
     glm::mat4 matrix;
     glm::vec4 vec;
     auto test = matrix * vec;
-
+    
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
@@ -31,5 +133,5 @@ int main() {
 
     glfwTerminate();
 
-    return 0;
+    return 0;*/
 }
